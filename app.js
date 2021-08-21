a6320b8a692b38751efdb12ff3ac04c07799a2a7
Ariful01303 <arifhossen01303@gmail.com>
2021-08-21 15:16:01 +0600

    complite my project

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+// product of price calculate 
 
 function getcostNumber(product,price){
     let productCost =document.getElementById(product +'-cost');
@@ -8,7 +8,7 @@ function getcostNumber(product,price){
     getTotal();
   
 }
-
+// Total price calculate 
 
 function getTotal(){
 
@@ -19,12 +19,13 @@ const memorytotal=document.getElementById('memory-cost').innerText;
 const storagetotal=document.getElementById('storage-cost').innerText;
 const deliverytotal=document.getElementById('delivery-cost').innerText;
 
-const totalprice =document.getElementById('total-cost').innerText= parseFloat (totalbest)+parseFloat(memorytotal) + parseFloat(storagetotal) + parseFloat(deliverytotal);
-const total = document.getElementById('total');
-const totalcoste =total.innerText;
-total.innerText=totalprice;
+const previousTotal =document.getElementById('total-cost').innerText= parseFloat (totalbest)+parseFloat(memorytotal) + parseFloat(storagetotal) + parseFloat(deliverytotal);
+const updateTotal = document.getElementById('total');
+const total =updateTotal.innerText;
+updateTotal.innerText=previousTotal;
 
 }
+// input promo code setup and change total amount 
 
 document.getElementById('promo-btn').addEventListener('click',function(){
     const promoCode =document.getElementById('promo-input');
@@ -46,38 +47,44 @@ document.getElementById('promo-btn').addEventListener('click',function(){
  
     
 })
+// memeory  evenhanglar
 
 const memory2Button =document.getElementById('memory-2ndbtn').addEventListener('click',function(){
-    // const memoryCost =document.getElementById('memory-cost').innerText=180;
+   
     getcostNumber('memory',180);
    
 })
 const memory1Button =document.getElementById('memory-firstbtn').addEventListener('click',function(){
-    // const memoryCost =document.getElementById('memory-cost').innerText=0;
+    
     getcostNumber('memory',0);
    
 })
+// storage  evenhanglar
+
 const storage1button =document.getElementById('storage-1stbtn').addEventListener('click',function(){
-    // const storageCost = document.getElementById('storage-cost').innerText=0;
+   
     getcostNumber('storage',0);
 })
 
 const storage2button =document.getElementById('storage-2ndbtn').addEventListener('click',function(){
-    // const storageCost = document.getElementById('storage-cost').innerText=100;
+  
     getcostNumber('storage',100);
 })
 
 const storage3button =document.getElementById('storage-3rdbtn').addEventListener('click',function(){
-    // const storageCost = document.getElementById('storage-cost').innerText=180;
+   
     getcostNumber('storage',180);
 })
+
+//DeliveryCost evenhanglar
+
 const delivery1btn = document.getElementById('delivery-1stbtn').addEventListener('click',function(){
-    // const deliveryCost =document.getElementById('delivery-cost').innerText=10;
+    
     getcostNumber('delivery',0);
 })
 
 const delivery2btn = document.getElementById('delivery-2ndbtn').addEventListener('click',function(){
-    // const deliveryCost =document.getElementById('delivery-cost').innerText= 20;
+    
     getcostNumber('delivery',20);
 })
 
